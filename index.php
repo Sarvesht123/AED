7774847fb979ce080f0916b30dd0c2ae2f29d9a7
Sarvesh Tiwari <sarvesht.aane@gmail.com>
2023-12-25 12:00:17 +0400

    updating main branch

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+/* start commenting from here at 22nd Dec 2023 from index branch */
+phpinfo();
+/* end commenting from here*/
+/* added comment on 22nd Dec 2023 from index branch */
+?>
---
+++ test.php
@@ -0,0 +1,5 @@
+<?php 
+/* adding comment at 22nd Dec 2023 updated from test branch */
+echo "Hello! Pipeline testing";
+/* ending comment at 22nd Dec 2023 updated from test branch */
+?>
